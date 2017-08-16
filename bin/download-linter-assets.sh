290938da25e6ddd36b70e9df31d200275b0295d8
Weston Ruter <weston@xwp.co>
2017-08-16 13:00:02 -0700

    Use direct GitHub URL for jsonlint.js

--- bin/download-linter-assets.sh
+++ bin/download-linter-assets.sh
@@ -6,4 +6,4 @@ mkdir -p wp-includes/js
 wget -O wp-includes/js/csslint.js http://csslint.net/js/csslint.js
 wget -O wp-includes/js/htmlhint.js http://htmlhint.com/js/htmlhint.js
 wget -O wp-includes/js/jshint.js //ajax.aspnetcdn.com/ajax/jshint/r07/jshint.js
-wget -O wp-includes/js/jsonlint.js https://rawgithub.com/zaach/jsonlint/79b553fb65c192add9066da64043458981b3972b/lib/jsonlint.js
+wget -O wp-includes/js/jsonlint.js https://raw.githubusercontent.com/zaach/jsonlint/master/lib/jsonlint.js
