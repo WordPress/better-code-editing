452f8f15d219d2ebfb3daff6de91cecf88191d61
Weston Ruter <weston@xwp.co>
2017-08-28 11:15:09 -0700

    Add Custom HTML help text to widgets admin screen

--- wp-includes/widgets/class-wp-widget-custom-html-codemirror.php
+++ wp-includes/widgets/class-wp-widget-custom-html-codemirror.php
@@ -44,6 +44,9 @@ class WP_Widget_Custom_HTML_CodeMirror extends WP_Widget_Custom_HTML {
 
 		// Note that the widgets component in the customizer will also do the 'admin_footer-widgets.php' action in WP_Customize_Widgets::print_footer_scripts().
 		add_action( 'admin_footer-widgets.php', array( 'WP_Widget_Custom_HTML_CodeMirror', 'render_control_template_scripts' ) );
+
+		// Note this action is used to ensure the help text is added to the end.
+		add_action( 'admin_head-widgets.php', array( 'WP_Widget_Custom_HTML_CodeMirror', 'add_help_text' ) );
 	}
 
 	/**
@@ -119,5 +122,44 @@ class WP_Widget_Custom_HTML_CodeMirror extends WP_Widget_Custom_HTML {
 			<?php endif; ?>
 		</script>
 		<?php
+	}
+
+	/**
+	 * Add help text to widgets admin screen.
+	 *
+	 * @since 4.9.0
+	 */
+	static public function add_help_text() {
+		$screen = get_current_screen();
+
+		$content = '<p>';
+		$content .= __( 'Use the Custom HTML widget to add arbitrary HTML code to your widget areas.', 'better-code-editing' );
+		$content .= '</p>';
+
+		$content .= '<p>' . __( 'When using a keyboard:', 'better-code-editing' ) . '</p>';
+		$content .= '<ul>';
+		$content .= '<li>' . __( 'In the HTML edit field, Tab enters a tab character.', 'better-code-editing' ) . '</li>';
+		$content .= '<li>' . __( 'To move keyboard focus, press Esc then Tab for the next element, or Esc then Shift+Tab for the previous element.', 'better-code-editing' ) . '</li>';
+		$content .= '</ul>';
+
+		$content .= '<p>';
+		$content .= sprintf(
+			/* translators: placeholder is link to user profile */
+			__( 'The edit field automatically highlights code syntax. You can disable this in your %s to work in plan text mode.', 'better-code-editing' ),
+			sprintf(
+				' <a href="%1$s" class="external-link" target="_blank">%2$s<span class="screen-reader-text">%3$s</span></a>',
+				esc_url( get_edit_profile_url() . '#syntax_highlighting' ),
+				__( 'user profile', 'better-code-editing' ),
+				/* translators: accessibility text */
+				__( '(opens in a new window)', 'default' )
+			)
+		);
+		$content .= '</p>';
+
+		$screen->add_help_tab( array(
+			'id' => 'custom_html_widget',
+			'title' => __( 'Custom HTML Widget', 'better-code-editing' ),
+			'content' => $content,
+		) );
 	}
 }
