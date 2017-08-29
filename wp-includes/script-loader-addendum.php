9146484d1bb8f99a3010aeed9b6d01338f54bc83
Weston Ruter <weston@xwp.co>
2017-08-29 15:00:15 -0700

    De-dup code into _better_code_editing_report_asset_errors

--- wp-includes/script-loader-addendum.php
+++ wp-includes/script-loader-addendum.php
@@ -122,37 +122,8 @@ function _better_code_editing_default_scripts( WP_Scripts $scripts ) {
 
 	$scripts->add( 'custom-html-widgets', plugins_url( 'wp-admin/js/widgets/custom-html-widgets.js', BETTER_CODE_EDITING_PLUGIN_FILE ), array( 'code-editor', 'jquery', 'backbone', 'wp-util' ), BETTER_CODE_EDITING_PLUGIN_VERSION );
 
-	// Make sure all CodeMirror assets present are registered. This will not be included in core merge.
 	if ( defined( 'SCRIPT_DEBUG' ) ) {
-		$plugin_dir_url = plugins_url( '', BETTER_CODE_EDITING_PLUGIN_FILE );
-		$codemirror_path = 'wp-includes/js/codemirror';
-		$codemirror_registered_paths = array();
-		$plugin_dir_path = plugin_dir_path( BETTER_CODE_EDITING_PLUGIN_FILE );
-
-		foreach ( $scripts->registered as $handle => $script ) {
-			if ( 0 === strpos( $script->src, $plugin_dir_url ) ) {
-				$path = substr( $script->src, strlen( $plugin_dir_url ) + 1 );
-				if ( ! file_exists( $plugin_dir_path . $path ) ) {
-					trigger_error( "Missing '$handle' script src: $path'", E_USER_WARNING );
-				}
-				if ( 0 === strpos( $path, $codemirror_path ) ) {
-					$codemirror_registered_paths[] = $path;
-				}
-			}
-		}
-
-		$directory = new RecursiveDirectoryIterator( $plugin_dir_path . $codemirror_path );
-		$iterator = new RecursiveIteratorIterator( $directory );
-		$js_iterator = new RegexIterator( $iterator, '#\.js$#' );
-
-		$codemirror_existing_paths = array();
-		foreach ( $js_iterator as $js_file ) {
-			$codemirror_existing_paths[] = substr( $js_file->getPathname(), strlen( $plugin_dir_path ) );
-		}
-		$unregistered_script_assets = array_diff( $codemirror_existing_paths, $codemirror_registered_paths );
-		if ( ! empty( $unregistered_script_assets ) ) {
-			trigger_error( sprintf( 'There are %d script assets from CodeMirror that are not registered: %s', count( $unregistered_script_assets ), join( ', ', $unregistered_script_assets ) ), E_USER_WARNING );
-		}
+		_better_code_editing_report_asset_errors( $scripts );
 	}
 }
 
@@ -187,40 +158,60 @@ function _better_code_editing_register_styles( WP_Styles $styles ) {
 	$styles->add_inline_style( 'widgets', file_get_contents( dirname( BETTER_CODE_EDITING_PLUGIN_FILE ) . '/wp-admin/css/widgets-addendum.css' ) );
 	$styles->add_inline_style( 'customize-controls', file_get_contents( dirname( BETTER_CODE_EDITING_PLUGIN_FILE ) . '/wp-admin/css/customize-controls-addendum.css' ) );
 
-	// Make sure all CodeMirror assets present are registered. This will not be included in core merge.
 	if ( defined( 'SCRIPT_DEBUG' ) ) {
-		$plugin_dir_url = plugins_url( '', BETTER_CODE_EDITING_PLUGIN_FILE );
-		$codemirror_path = 'wp-includes/js/codemirror';
-		$codemirror_registered_paths = array();
-		$plugin_dir_path = plugin_dir_path( BETTER_CODE_EDITING_PLUGIN_FILE );
+		_better_code_editing_report_asset_errors( $styles );
+	}
+}
 
-		foreach ( $styles->registered as $handle => $style ) {
-			if ( 'common' === $handle ) {
-				continue;
-			}
+/**
+ * Make sure all CodeMirror assets present are registered.
+ *
+ * This will not be included in core merge.
+ *
+ * @param WP_Scripts|WP_Styles $dependencies Dependencies.
+ */
+function _better_code_editing_report_asset_errors( $dependencies ) {
+	$plugin_dir_url = plugins_url( '', BETTER_CODE_EDITING_PLUGIN_FILE );
+	$codemirror_path = 'wp-includes/js/codemirror';
+	$codemirror_registered_paths = array();
+	$plugin_dir_path = plugin_dir_path( BETTER_CODE_EDITING_PLUGIN_FILE );
+	$type = $dependencies instanceof WP_Styles ? 'style' : 'script';
 
-			if ( 0 === strpos( $style->src, $plugin_dir_url ) ) {
-				$path = substr( $style->src, strlen( $plugin_dir_url ) + 1 );
-				if ( ! file_exists( $plugin_dir_path . $path ) ) {
-					trigger_error( "Missing '$handle' style src: $path'", E_USER_WARNING );
-				}
-				if ( 0 === strpos( $path, $codemirror_path ) ) {
-					$codemirror_registered_paths[] = $path;
-				}
-			}
+	foreach ( $dependencies->registered as $handle => $style ) {
+		if ( 'common' === $handle ) {
+			continue;
 		}
 
-		$directory = new RecursiveDirectoryIterator( $plugin_dir_path . $codemirror_path );
-		$iterator = new RecursiveIteratorIterator( $directory );
-		$css_iterator = new RegexIterator( $iterator, '#\.css$#' );
-
-		$codemirror_existing_paths = array();
-		foreach ( $css_iterator as $js_file ) {
-			$codemirror_existing_paths[] = substr( $js_file->getPathname(), strlen( $plugin_dir_path ) );
-		}
-		$unregistered_assets = array_diff( $codemirror_existing_paths, $codemirror_registered_paths );
-		if ( ! empty( $unregistered_assets ) ) {
-			trigger_error( sprintf( 'There are %d style assets from CodeMirror that are not registered: %s', count( $unregistered_assets ), join( ', ', $unregistered_assets ) ), E_USER_WARNING );
+		if ( 0 === strpos( $style->src, $plugin_dir_url ) ) {
+			$path = substr( $style->src, strlen( $plugin_dir_url ) + 1 );
+			if ( ! file_exists( $plugin_dir_path . $path ) ) {
+				trigger_error( "Missing '$handle' $type src: $path'", E_USER_WARNING );
+			}
+			if ( 0 === strpos( $path, $codemirror_path ) ) {
+				$codemirror_registered_paths[] = $path;
+			}
 		}
 	}
+
+	$directory = new RecursiveDirectoryIterator( $plugin_dir_path . $codemirror_path );
+	$iterator = new RecursiveIteratorIterator( $directory );
+	$file_iterator = new RegexIterator( $iterator, $dependencies instanceof WP_Styles ? '#\.css$#' : '#\.js$#' );
+
+	$codemirror_existing_paths = array();
+	foreach ( $file_iterator as $js_file ) {
+		$codemirror_existing_paths[] = substr( $js_file->getPathname(), strlen( $plugin_dir_path ) );
+	}
+	$unregistered_assets = array_diff( $codemirror_existing_paths, $codemirror_registered_paths );
+	if ( ! empty( $unregistered_assets ) ) {
+		trigger_error(
+			sprintf(
+				'There are %d %s assets from CodeMirror that are not registered: %s',
+				count( $unregistered_assets ),
+				$type,
+				join( ', ', $unregistered_assets )
+			),
+			E_USER_WARNING
+		);
+	}
+
 }
